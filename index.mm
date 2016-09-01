852d52c7c6878d6c4d9da5af7c4c9ec551695388
Hui Zhu <zhuhui@xiaomi.com>
2016-09-01 14:06:50 +0800

    Use mindmap generate html

---
+++ index.mm
@@ -0,0 +1,22 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1472708408515" ID="ID_662009073" MODIFIED="1472708431776" TEXT="teawater(Hui Zhu)&apos;s Homepage">
+<node CREATED="1472708752837" ID="ID_583425608" LINK="indexcn.html" MODIFIED="1472709263999" POSITION="right" TEXT="Chinese"/>
+<node CREATED="1472708596511" ID="ID_1638893491" MODIFIED="1472708601725" POSITION="left" TEXT="My Projects">
+<node CREATED="1472708440400" ID="ID_756456714" LINK="http://teawater.github.io/kgtp/" MODIFIED="1472708503827" TEXT="KGTP">
+<node CREATED="1472708553887" ID="ID_477053151" MODIFIED="1472708554707" TEXT="KGTP is a comprehensive dynamic tracer for analysing Linux kernel and application (including Android) problems on production systems in real time.]\"/>
+</node>
+<node CREATED="1472708619957" ID="ID_1905391322" LINK="http://teawater.github.io/cma/" MODIFIED="1472708652309" TEXT="CMA">
+<node CREATED="1472708659623" ID="ID_438199411" MODIFIED="1472708661619" TEXT="A GDB Python script that analyzes and records C/C++ application&apos;s dynamic memory status. "/>
+</node>
+<node CREATED="1472708672687" ID="ID_1029802733" LINK="http://teawater.github.io/get-gdb/" MODIFIED="1472708691196" TEXT="Get-GDB">
+<node CREATED="1472708696477" ID="ID_420345347" MODIFIED="1472708719629" TEXT="A Python script to auto check version, download, build and install specified version GDB to current system."/>
+</node>
+</node>
+<node CREATED="1472709297884" ID="ID_1226999614" MODIFIED="1472709396321" POSITION="right" TEXT="My mindmap">
+<node CREATED="1472709684271" ID="ID_1955654431" LINK="mindmap/Code_Simplicity.html" MODIFIED="1472709912117" TEXT="&#x7b80;&#x7ea6;&#x4e4b;&#x7f8e;&#xff1a;&#x8f6f;&#x4ef6;&#x8bbe;&#x8ba1;&#x4e4b;&#x9053;"/>
+</node>
+<node CREATED="1472709817072" ID="ID_1649698738" LINK="https://github.com/teawater" MODIFIED="1472709828126" POSITION="left" TEXT="My github"/>
+<node CREATED="1472709833438" ID="ID_851068096" LINK="http://www.linkedin.com/in/teawater" MODIFIED="1472709852261" POSITION="right" TEXT="My Linkedin"/>
+</node>
+</map>
