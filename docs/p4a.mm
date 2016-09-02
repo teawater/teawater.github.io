731d5965af6e0fa859977760f51f69a14971b763
Hui Zhu <zhuhui@xiaomi.com>
2016-09-02 16:12:06 +0800

    New file for p4a

---
+++ docs/p4a.mm
@@ -0,0 +1,124 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1472790239794" ID="ID_985310946" MODIFIED="1472793994812" TEXT="&#x5728;&#x5b89;&#x5353;&#x5e73;&#x53f0;&#x4e0a;&#x4f7f;&#x7528;Python&#x7684;&#x65b9;&#x6cd5;">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1472791072318" ID="ID_1561407206" MODIFIED="1472794092526" POSITION="right" TEXT="&#x5b89;&#x88c5;QPython&#x7684;APK">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1472792261095" ID="ID_274970591" MODIFIED="1472793994808" TEXT="&#x6211;&#x89c2;&#x5bdf;&#x5230;&#x7684;&#x4f18;&#x70b9;&#x3002;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1472792270750" ID="ID_372551516" MODIFIED="1472793994808" TEXT="&#x76f4;&#x63a5;&#x53ef;&#x7528;&#x3002;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472792390590" ID="ID_1997100422" MODIFIED="1472793994808" TEXT="&#x652f;&#x6301;&#x547d;&#x4ee4;&#x884c;&#x6a21;&#x5f0f;&#xff0c;&#x5f88;&#x591a;&#x4ee3;&#x7801;&#x53ef;&#x4ee5;&#x653e;&#x4e0a;&#x53bb;&#x5c31;&#x8dd1;&#x3002;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472792811911" ID="ID_1500520476" MODIFIED="1472793994809" TEXT="&#x652f;&#x6301;&#x6846;&#x67b6;&#x3002;">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1472792911849" ID="ID_1616815898" MODIFIED="1472793994809" TEXT="Kivy">
+<node COLOR="#111111" CREATED="1472793127863" ID="ID_386834528" MODIFIED="1472793994809" TEXT="&#x5b83;&#x63d0;&#x4f9b;&#x7684;&#x4ee3;&#x7801;&#x53ef;&#x8fd0;&#x884c;&#x3002;"/>
+<node COLOR="#111111" CREATED="1472793153039" ID="ID_710205593" MODIFIED="1472793994809" TEXT="&#x4f46;&#x662f;&#x6211;&#x81ea;&#x5df1;&#x5199;&#x4e86;&#x4e2a;&#x7a0b;&#x5e8f;&#x653e;&#x4e0a;&#x53bb;&#x8fd0;&#x884c;&#xff0c;&#x8fd9;&#x4e2a;&#x7a0b;&#x5e8f;&#x6ca1;&#x6709;&#x6b63;&#x786e;&#x6267;&#x884c;&#x3002;"/>
+</node>
+<node COLOR="#111111" CREATED="1472793104689" ID="ID_566775279" MODIFIED="1472793994809" TEXT="Web App&#x3002;"/>
+</node>
+<node COLOR="#990000" CREATED="1472793006215" ID="ID_1591226878" MODIFIED="1472793994809" TEXT="&#x81ea;&#x5e26;IDE&#xff0c;&#x53ef;&#x4ee5;&#x5728;&#x624b;&#x673a;&#x4e0a;&#x76f4;&#x63a5;&#x7f16;&#x5199;&#x4ee3;&#x7801;&#x3002;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472793249023" ID="ID_324657226" MODIFIED="1472793994810" TEXT="&#x53ef;&#x4ee5;&#x901a;&#x8fc7;&#x4e8c;&#x7ef4;&#x7801;&#x5411;&#x91cc;&#x9762;&#x4f20;&#x8f93;&#x4ee3;&#x7801;&#x3002;">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1472793323047" ID="ID_500065371" LINK="http://qpython.com/#qrcode" MODIFIED="1472793994810" TEXT="http://qpython.com/#qrcode"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1472792938679" ID="ID_1578620262" MODIFIED="1472793994810" TEXT="&#x6211;&#x89c2;&#x5bdf;&#x7684;&#x7f3a;&#x70b9;&#x3002;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1472792948784" ID="ID_252199949" MODIFIED="1472793994811" TEXT="&#x6709;&#x4e9b;&#x5e93;&#x53ef;&#x80fd;&#x6ca1;&#x6709;&#x3002;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472793224432" ID="ID_549452281" MODIFIED="1472793994811" TEXT="&#x4e0d;&#x662f;APK&#xff0c;&#x7a0b;&#x5e8f;&#x7684;&#x53d1;&#x5e03;&#x4e0d;&#x4fbf;&#xff0c;&#x611f;&#x89c9;&#x4e0d;&#x591f;&#x4e13;&#x4e1a;&#x3002;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1472793709240" ID="ID_1760145622" MODIFIED="1472803708734" POSITION="left" TEXT="&#x76f4;&#x63a5;&#x7528;Kivy&#x73af;&#x5883;&#x628a;&#x7a0b;&#x5e8f;&#x7f16;&#x8bd1;&#x6210;APK">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1472794110097" ID="ID_1291614699" MODIFIED="1472794117429" TEXT="&#x6211;&#x89c2;&#x5bdf;&#x5230;&#x7684;&#x4f18;&#x70b9;&#x3002;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1472794120392" ID="ID_491969948" MODIFIED="1472794136333" TEXT="Python&#x4ee3;&#x7801;&#x53ef;&#x4ee5;&#x7f16;&#x8bd1;&#x6210;APK&#x53d1;&#x5e03;&#x3002;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472794142042" ID="ID_987094112" MODIFIED="1472794709342" TEXT="Kivy&#x652f;&#x6301;&#x591a;&#x79cd;&#x5e73;&#x53f0;ISO&#xff0c;&#x5b89;&#x5353;&#xff0c;LINUX&#xff0c;WIN&#x3002;">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1472794713754" ID="ID_836079432" MODIFIED="1472794735173" TEXT="&#x6211;&#x5b9e;&#x9645;&#x6d4b;&#x8bd5;&#x4e86;&#x540c;&#x6837;&#x7684;&#x4ee3;&#x7801;&#x53ef;&#x4ee5;&#x5728;UBUNTU&#x548c;&#x5b89;&#x5353;&#x4e0a;&#x4f7f;&#x7528;&#x3002;"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1472794744753" ID="ID_436838003" MODIFIED="1472794750805" TEXT="&#x6211;&#x89c2;&#x5bdf;&#x5230;&#x7684;&#x7f3a;&#x70b9;&#x3002;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1472794753312" ID="ID_1804225345" MODIFIED="1472794799365" TEXT="&#x5b89;&#x5353;&#x7f16;&#x8bd1;&#x73af;&#x5883;&#x642d;&#x5efa;&#x8f83;&#x590d;&#x6742;&#x3002;">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1472794802273" ID="ID_619072591" MODIFIED="1472794861165" TEXT="&#x4e3a;&#x4e86;&#x89e3;&#x51b3;&#x8fd9;&#x4e2a;&#x95ee;&#x9898;Kivy&#x4e13;&#x95e8;&#x63d0;&#x4f9b;&#x4e86;VBOX&#x955c;&#x50cf;&#x3002;"/>
+<node COLOR="#111111" CREATED="1472794871434" ID="ID_1336190177" MODIFIED="1472794897386" TEXT="&#x5b9e;&#x9645;&#x4f7f;&#x7528;&#x6b63;&#x786e;&#x65b9;&#x6cd5;&#x5728;Ubuntu&#x4e0a;&#x8fd8;&#x662f;&#x5f88;&#x597d;&#x642d;&#x5efa;&#x7684;&#xff0c;&#x540e;&#x9762;&#x6211;&#x4f1a;&#x4ecb;&#x7ecd;&#x5230;&#x3002;"/>
+</node>
+<node COLOR="#990000" CREATED="1472794927713" ID="ID_1284686318" MODIFIED="1472794942198" TEXT="&#x9700;&#x8981;&#x638c;&#x63e1;&#x5bf9;Kivy&#x5e93;&#x7684;&#x4f7f;&#x7528;&#x3002;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1472794982801" ID="ID_1234429935" MODIFIED="1472795128158" TEXT="&#x5b89;&#x5353;&#x539f;&#x751f;&#x5f00;&#x53d1;&#x65b9;&#x6cd5;&#x8fd8;&#x662f;&#x7528;JAVA&#xff0c;&#x8fd9;&#x4e2a;&#x73af;&#x5883;&#x80fd;&#x53d1;&#x5c55;&#x5230;&#x4ec0;&#x4e48;&#x9636;&#x6bb5;&#x8fd8;&#x6709;&#x5f85;&#x89c2;&#x5bdf;&#x3002;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1472794953184" ID="ID_1848871892" MODIFIED="1472795555022" TEXT="&#x5b89;&#x5353;&#x7f16;&#x8bd1;&#x73af;&#x5883;&#x914d;&#x7f6e;&#x548c;&#x4f7f;&#x7528;&#x3002;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1472795156281" ID="ID_404457762" MODIFIED="1472800968755" TEXT="&#x7528;python-for-android&#x73af;&#x5883;">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1472801028310" ID="ID_1404585585" LINK="https://python-for-android.readthedocs.io/en/latest/quickstart/" MODIFIED="1472801048144" TEXT="https://python-for-android.readthedocs.io/en/latest/quickstart/"/>
+<node COLOR="#111111" CREATED="1472800973767" ID="ID_1168427590" MODIFIED="1472801080763" TEXT="&#x641c;&#x5230;&#x7684;&#x5173;&#x4e8e;Kivy&#x7684;&#x4e2d;&#x6587;&#x5b89;&#x88c5;&#x5c31;&#x4f1a;&#x641c;&#x5230;&#x8fd9;&#x4e2a;&#x65b9;&#x6cd5;&#xff0c;&#x6211;&#x7528;&#x8fc7;&#xff0c;&#x5341;&#x5206;&#x9ebb;&#x70e6;&#x4e14;&#x5230;&#x6700;&#x540e;&#x7f16;&#x8bd1;&#x5931;&#x8d25;&#x3002;"/>
+<node COLOR="#111111" CREATED="1472801121414" ID="ID_1500081102" MODIFIED="1472801130760" TEXT="&#x5f3a;&#x70c8;&#x4e0d;&#x63a8;&#x8350;&#x3002;"/>
+</node>
+<node COLOR="#990000" CREATED="1472801100398" ID="ID_1356898113" MODIFIED="1472801162139" TEXT="&#x7528;Kivy&#x63d0;&#x4f9b;&#x7684;&#x865a;&#x62df;&#x673a;&#x955c;&#x50cf;&#x3002;">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1472801168670" ID="ID_1506280736" LINK="https://kivy.org/docs/guide/packaging-android-vm.html" MODIFIED="1472801185725" TEXT="https://kivy.org/docs/guide/packaging-android-vm.html"/>
+<node COLOR="#111111" CREATED="1472801846855" ID="ID_1030945182" MODIFIED="1472802299660" TEXT="&#x6211;&#x5728;VBOX&#x4e0a;&#x7528;&#x4e86;&#x4e0b;&#x662f;&#x80fd;&#x8dd1;&#x7684;&#xff0c;&#x4f46;&#x6ca1;&#x7f16;&#x8bd1;APK&#x3002;"/>
+<node COLOR="#111111" CREATED="1472801875569" ID="ID_1331225416" MODIFIED="1472802447056" TEXT="&#x5efa;&#x8bae;Buildozer&#x4e0d;&#x80fd;&#x7528;&#x518d;&#x7528;&#x8fd9;&#x4e2a;&#x65b9;&#x6cd5;&#x3002;"/>
+<node COLOR="#111111" CREATED="1472802262136" ID="ID_1646916364" MODIFIED="1472802321960" TEXT="&#x5177;&#x4f53;&#x4f7f;&#x7528;&#x65b9;&#x6cd5;&#x548c;Buildozer&#x4e00;&#x6837;&#x3002;"/>
+</node>
+<node COLOR="#990000" CREATED="1472802351951" ID="ID_351886228" MODIFIED="1472802421724" TEXT="&#x5728;LINUX&#x4e0b;&#x7528;&#x81ea;&#x52a8;&#x811a;&#x672c;Buildozer">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1472802473191" ID="ID_730540305" LINK="https://kivy.org/docs/guide/packaging-android.html" MODIFIED="1472802490502" TEXT="https://kivy.org/docs/guide/packaging-android.html"/>
+<node COLOR="#111111" CREATED="1472802494327" ID="ID_102537909" MODIFIED="1472802520588" TEXT="&#x6211;&#x5728;LINUX&#x4e0b;&#x7528;&#x4e86;&#x8fd9;&#x4e2a;&#x65b9;&#x6cd5;&#xff0c;&#x5b8c;&#x5168;&#x53ef;&#x7528;&#x6ca1;&#xff0c;&#x5f88;&#x65b9;&#x4fbf;&#x3002;"/>
+<node COLOR="#111111" CREATED="1472802550911" ID="ID_1755085110" MODIFIED="1472802559285" TEXT="&#x914d;&#x7f6e;">
+<node COLOR="#111111" CREATED="1472802568640" ID="ID_1574332311" MODIFIED="1472802576394" TEXT="git clone https://github.com/kivy/buildozer.git&#xa;cd buildozer&#xa;sudo python2.7 setup.py install">
+<node COLOR="#111111" CREATED="1472803798813" ID="ID_1278321075" MODIFIED="1472803846009" TEXT="&#x6700;&#x540e;&#x7684;&#x547d;&#x4ee4;&#x4f1a;&#x81ea;&#x52a8;&#x4e0b;&#x8f7d;&#x9700;&#x8981;&#x7684;&#x8f6f;&#x4ef6;&#x5305;&#x62ec;Android SDK&#x548c;NDK&#x3002;"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1472802665664" ID="ID_1111171440" MODIFIED="1472802669613" TEXT="&#x4f7f;&#x7528;">
+<node COLOR="#111111" CREATED="1472802679839" ID="ID_1918416123" MODIFIED="1472802713076" TEXT="&#x9879;&#x76ee;&#x521d;&#x59cb;&#x5316;&#x3002;">
+<node COLOR="#111111" CREATED="1472802719242" ID="ID_621345916" MODIFIED="1472802865292" TEXT="&#x8fdb;&#x5165;&#x9879;&#x76ee;&#x76ee;&#x5f55;&#x3002;"/>
+<node COLOR="#111111" CREATED="1472802735407" ID="ID_833791122" MODIFIED="1472802736476" TEXT="buildozer init"/>
+</node>
+<node COLOR="#111111" CREATED="1472802791863" ID="ID_1586214864" MODIFIED="1472802798231" TEXT="&#x9879;&#x76ee;&#x914d;&#x7f6e;&#x3002;">
+<node COLOR="#111111" CREATED="1472802826247" ID="ID_1720033239" MODIFIED="1472802991127" TEXT="&#x9879;&#x76ee;&#x76ee;&#x5f55;&#x6587;&#x4ef6;buildozer.spec&#x5c31;&#x662f;&#x914d;&#x7f6e;&#x6587;&#x4ef6;&#xff0c;&#x4ece;&#x6ce8;&#x91ca;&#x53ef;&#x4ee5;&#x770b;&#x5230;&#x9ed8;&#x8ba4;&#x4ee3;&#x7801;&#x6587;&#x4ef6;&#x662f;&#x9879;&#x76ee;&#x76ee;&#x5f55;&#x4e2d;&#x7684;main.py&#x3002;"/>
+</node>
+<node COLOR="#111111" CREATED="1472802994656" ID="ID_1769698031" MODIFIED="1472803001488" TEXT="&#x9879;&#x76ee;&#x7f16;&#x7801;&#x3002;">
+<node COLOR="#111111" CREATED="1472803004128" ID="ID_1119040811" MODIFIED="1472803214218" TEXT="&#x7f16;&#x8f91;&#x6587;&#x4ef6;main.py&#xa;import kivy&#xa;kivy.require(&apos;1.4.1&apos;)&#xa;&#xa;from kivy.app import App&#xa;from kivy.uix.button import Button&#xa;&#xa;class HeiBoyApp(App):&#xa;    &quot;&quot;&quot;docstring for MyApp&quot;&quot;&quot;&#xa;    def build(self):&#xa;        return Button(text=&apos;Hello World&apos;)&#xa;&#xa;if __name__ == &apos;__main__&apos;:&#xa;   HeiBoyApp().run()&#xa;"/>
+<node COLOR="#111111" CREATED="1472803261256" ID="ID_1678854009" MODIFIED="1472803279716" TEXT="&#x6ce8;&#x610f;&#x8fd9;&#x4e2a;&#x4ee3;&#x7801;&#x5728;&#x914d;&#x7f6e;&#x597d;kivy&#x7684;&#x673a;&#x5668;&#x90fd;&#x76f4;&#x63a5;&#x53ef;&#x7528;&#x7684;&#x3002;"/>
+</node>
+<node COLOR="#111111" CREATED="1472803288640" ID="ID_1970130624" MODIFIED="1472803293509" TEXT="&#x9879;&#x76ee;&#x7f16;&#x8bd1;&#x3002;">
+<node COLOR="#111111" CREATED="1472803315288" ID="ID_1247011340" MODIFIED="1472803316462" TEXT="buildozer android debug">
+<node COLOR="#111111" CREATED="1472803325552" ID="ID_294053129" MODIFIED="1472803349998" TEXT="&#x7136;&#x540e;apk&#x5c31;&#x4f1a;&#x51fa;&#x73b0;&#x5728;&#x9879;&#x76ee;&#x76ee;&#x5f55;&#x7684;bin&#x76ee;&#x5f55;&#x4e86;&#x3002;"/>
+</node>
+<node COLOR="#111111" CREATED="1472803363522" ID="ID_1555086722" MODIFIED="1472803364488" TEXT="buildozer android debug deploy run">
+<node COLOR="#111111" CREATED="1472803367489" ID="ID_1064830845" MODIFIED="1472803382861" TEXT="&#x589e;&#x52a0;&#x4e86;&#x8fd9;&#x4e24;&#x4e2a;&#x9009;&#x9879;&#x8fd8;&#x4f1a;&#x81ea;&#x52a8;&#x90e8;&#x7f72;&#x548c;&#x8fd0;&#x884c;&#x3002;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
