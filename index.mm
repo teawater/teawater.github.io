4eaec21dd70a9e4d46bc70710ea68fe472c5bc0b
Hui Zhu <zhuhui@xiaomi.com>
2016-09-20 16:59:08 +0800

    swich en and cn

--- index.mm
+++ index.mm
@@ -1,40 +1,40 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#000000" CREATED="1472708408515" ID="ID_662009073" MODIFIED="1473754198367" TEXT="teawater(Hui Zhu)&apos;s Homepage">
+<node COLOR="#000000" CREATED="1472708408515" ID="ID_662009073" MODIFIED="1472804205577" TEXT="teawater(&#x6731;&#x8f89;)&#x7684;&#x4e3b;&#x9875;">
 <font NAME="SansSerif" SIZE="20"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties"/>
-<node COLOR="#0033ff" CREATED="1472708752837" ID="ID_583425608" LINK="indexcn.html" MODIFIED="1474173591077" POSITION="right" TEXT="&#x4e2d;&#x6587;">
+<node COLOR="#0033ff" CREATED="1472708752837" ID="ID_583425608" LINK="indexen.html" MODIFIED="1474361625671" POSITION="right" TEXT="English">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1472708596511" ID="ID_1638893491" MODIFIED="1472804071716" POSITION="left" TEXT="My Projects">
+<node COLOR="#0033ff" CREATED="1472708596511" ID="ID_1638893491" MODIFIED="1472804205566" POSITION="left" TEXT="&#x6211;&#x7684;&#x9879;&#x76ee;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1474351738446" ID="ID_487449301" LINK="https://lkml.org/lkml/2016/8/22/151" MODIFIED="1474351845114" TEXT="Make ZRAM just store the high compression rate page">
+<node COLOR="#00b439" CREATED="1474351738446" ID="ID_487449301" LINK="https://lkml.org/lkml/2016/8/22/151" MODIFIED="1474351854418" TEXT="&#x8ba9;ZRAM&#x53ea;&#x50a8;&#x5b58;&#x9ad8;&#x538b;&#x7f29;&#x7387;&#x7684;&#x9875;&#x9762;">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 </node>
-<node COLOR="#00b439" CREATED="1472708440400" ID="ID_756456714" LINK="http://teawater.github.io/kgtp/" MODIFIED="1474351435507" TEXT="KGTP">
+<node COLOR="#00b439" CREATED="1472708440400" ID="ID_756456714" LINK="http://teawater.github.io/kgtp/" MODIFIED="1474351453379" TEXT="KGTP">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1472708553887" ID="ID_477053151" MODIFIED="1474351425093" TEXT="KGTP is a comprehensive dynamic tracer for analysing Linux kernel and application (including Android) problems on production systems in real time.">
+<node COLOR="#990000" CREATED="1472708553887" ID="ID_477053151" MODIFIED="1472804205567" TEXT="KGTP &#x662f;&#x4e00;&#x4e2a;&#x80fd;&#x5728;&#x4ea7;&#x54c1;&#x7cfb;&#x7edf;&#x4e0a;&#x5b9e;&#x65f6;&#x5206;&#x6790;Linux&#x5185;&#x6838;&#x548c;&#x5e94;&#x7528;&#x7a0b;&#x5e8f;(&#x5305;&#x62ec;Android)&#x95ee;&#x9898;&#x7684;&#x5168;&#x9762;&#x52a8;&#x6001;&#x8ddf;&#x8e2a;&#x5668;&#x3002;">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1474346942731" ID="ID_207662382" MODIFIED="1474347490133" TEXT="GDB Reverse Debug and Process Record">
+<node COLOR="#00b439" CREATED="1474346942731" ID="ID_207662382" MODIFIED="1474351457721" TEXT="GDB&#x53cd;&#x5411;&#x8c03;&#x8bd5;&#x548c;&#x8bb0;&#x5f55;&#x8c03;&#x8bd5;">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1474347420888" ID="ID_905890041" LINK="https://www.gnu.org/software/gdb/news/reversible.html" MODIFIED="1474347453132" TEXT="GDB and Reverse Debugging">
+<node COLOR="#990000" CREATED="1474347420888" ID="ID_905890041" LINK="https://www.gnu.org/software/gdb/news/reversible.html" MODIFIED="1474347776469" TEXT="GDB&#x53cd;&#x5411;&#x8c03;&#x8bd5;">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
-<node COLOR="#990000" CREATED="1474347428520" ID="ID_1354850590" LINK="https://sourceware.org/gdb/wiki/ProcessRecord" MODIFIED="1474347465059" TEXT="Process Record and Replay">
+<node COLOR="#990000" CREATED="1474347428520" ID="ID_1354850590" LINK="https://sourceware.org/gdb/wiki/ProcessRecord" MODIFIED="1474347788621" TEXT="GDB&#x8bb0;&#x5f55;&#x8c03;&#x8bd5;">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
-<node COLOR="#990000" CREATED="1474347438184" ID="ID_1211499164" LINK="prec/prec.pdf" MODIFIED="1474347479591" TEXT="slide">
+<node COLOR="#990000" CREATED="1474347438184" ID="ID_1211499164" LINK="prec/prec.pdf" MODIFIED="1474347796477" TEXT="&#x5e7b;&#x706f;&#x7247;">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1474338938608" ID="ID_575319148" MODIFIED="1474351440905" TEXT="2006: Skyeye DBCT">
+<node COLOR="#00b439" CREATED="1474338938608" ID="ID_575319148" MODIFIED="1474347723509" TEXT="2006: Skyeye&#x6307;&#x4ee4;&#x52a8;&#x6001;&#x7ffb;&#x8bd1;&#x6a21;&#x62df;">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
 <node COLOR="#990000" CREATED="1474339609600" ID="ID_949165" LINK="dbct/skyeye_dbct_doc.html" MODIFIED="1474340772023" TEXT="SKYEYE&#x6307;&#x4ee4;&#x52a8;&#x6001;&#x7ffb;&#x8bd1;&#x6a21;&#x62df;(DBCT)&#x5b9e;&#x73b0;&#x4ecb;&#x7ecd;">
@@ -44,33 +44,33 @@
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1472708672687" ID="ID_1029802733" LINK="http://teawater.github.io/get-gdb/" MODIFIED="1472804071730" TEXT="Get-GDB">
+<node COLOR="#00b439" CREATED="1472708672687" ID="ID_1029802733" LINK="http://teawater.github.io/get-gdb/" MODIFIED="1472804205573" TEXT="Get-GDB">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1472708696477" ID="ID_420345347" MODIFIED="1472804071731" TEXT="A Python script to auto check version, download, build and install specified version GDB to current system.">
+<node COLOR="#990000" CREATED="1472708696477" ID="ID_420345347" MODIFIED="1472804205573" TEXT="&#x53ef;&#x4ee5;&#x81ea;&#x52a8;&#x68c0;&#x67e5;&#x7248;&#x672c;&#xff0c;&#x4e0b;&#x8f7d;&#xff0c;&#x7f16;&#x8bd1;&#xff0c;&#x5b89;&#x88c5;&#x6307;&#x5b9a;&#x7248;&#x672c;GDB&#x5230;&#x5f53;&#x524d;&#x7cfb;&#x7edf;&#x7684;Python&#x811a;&#x672c;&#x3002;">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1472708619957" ID="ID_1905391322" LINK="http://teawater.github.io/cma/" MODIFIED="1474341750911" TEXT="CMA">
+<node COLOR="#00b439" CREATED="1472708619957" ID="ID_1905391322" LINK="http://teawater.github.io/cma/" MODIFIED="1474341759431" TEXT="CMA">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1472708659623" ID="ID_438199411" MODIFIED="1472804071729" TEXT="A GDB Python script that analyzes and records C/C++ application&apos;s dynamic memory status. ">
+<node COLOR="#990000" CREATED="1472708659623" ID="ID_438199411" MODIFIED="1472804205572" TEXT="&#x5206;&#x6790;&#x548c;&#x8bb0;&#x5f55;C/C++&#x7a0b;&#x5e8f;&#x52a8;&#x6001;&#x5185;&#x5b58;&#x72b6;&#x6001;&#x7684;GDB Python&#x811a;&#x672c;&#x3002;">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1473754199442" ID="ID_452624323" MODIFIED="1473754251968" POSITION="right" TEXT="My presentation">
+<node COLOR="#0033ff" CREATED="1473754199442" ID="ID_452624323" MODIFIED="1473754456134" POSITION="right" TEXT="&#x6211;&#x7684;&#x6f14;&#x8bb2;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1473754297417" ID="ID_823409592" MODIFIED="1474168607106" TEXT="2014: Buddy&#x548c;CMA&#x7b80;&#x4ecb; &#x4ee5;&#x53ca;&#x5728;Android&#x4e2d;&#x5b9e;&#x9645;&#x4f7f;&#x7528; CMA&#x9047;&#x5230;&#x95ee;&#x9898;&#x7684;&#x6539;&#x8fdb;">
+<node COLOR="#00b439" CREATED="1473754297417" ID="ID_823409592" MODIFIED="1473754298876" TEXT="Buddy&#x548c;CMA&#x7b80;&#x4ecb; &#x4ee5;&#x53ca;&#x5728;Android&#x4e2d;&#x5b9e;&#x9645;&#x4f7f;&#x7528; CMA&#x9047;&#x5230;&#x95ee;&#x9898;&#x7684;&#x6539;&#x8fdb;">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1473754338481" ID="ID_730958682" LINK="presentation/2014clk_cma.pdf" MODIFIED="1473754376673" TEXT="slide">
+<node COLOR="#990000" CREATED="1473754338481" ID="ID_730958682" LINK="presentation/2014clk_cma.pdf" MODIFIED="1473831373561" TEXT="&#x5e7b;&#x706f;&#x7247;">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
-<node COLOR="#990000" CREATED="1473831335355" ID="ID_120183860" MODIFIED="1473831339091" TEXT="video">
+<node COLOR="#990000" CREATED="1473831379356" ID="ID_729585102" MODIFIED="1473831382744" TEXT="&#x89c6;&#x9891;">
 <font NAME="SansSerif" SIZE="14"/>
-<node COLOR="#111111" CREATED="1473755069350" ID="ID_260329777" LINK="https://youtu.be/jMhDBTdpfG0" MODIFIED="1473831349843" TEXT="youtube">
+<node COLOR="#111111" CREATED="1473755069350" ID="ID_260329777" LINK="https://youtu.be/jMhDBTdpfG0" MODIFIED="1473831388244" TEXT="youtube">
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 <node COLOR="#111111" CREATED="1473831319792" ID="ID_225778867" LINK="http://v.youku.com/v_show/id_XMTcyMzk5Mzc4MA==.html" MODIFIED="1473831349844" TEXT="youku">
@@ -79,7 +79,7 @@
 </node>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1472709297884" ID="ID_1226999614" MODIFIED="1472804071734" POSITION="right" TEXT="My mindmap">
+<node COLOR="#0033ff" CREATED="1472709297884" ID="ID_1226999614" MODIFIED="1472804205574" POSITION="right" TEXT="&#x6211;&#x7684;&#x601d;&#x7ef4;&#x5bfc;&#x56fe;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
 <node COLOR="#00b439" CREATED="1474168934713" ID="ID_1441026216" MODIFIED="1474168940500" TEXT="&#x7b80;&#x7ea6;&#x4e4b;&#x7f8e;&#xff1a;&#x8f6f;&#x4ef6;&#x8bbe;&#x8ba1;&#x4e4b;&#x9053; &#x8bfb;&#x4e66;&#x7b14;&#x8bb0;">
@@ -105,22 +105,22 @@
 </node>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1472803961041" ID="ID_784008993" MODIFIED="1472804134439" POSITION="left" TEXT="My documents">
+<node COLOR="#0033ff" CREATED="1472804212289" ID="ID_184369074" MODIFIED="1472804268077" POSITION="left" TEXT="&#x6211;&#x7684;&#x6587;&#x6863;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
-<node COLOR="#00b439" CREATED="1474349523443" ID="ID_1281547563" MODIFIED="1474349533374" TEXT="Porting GDB">
+<node COLOR="#00b439" CREATED="1474349523443" ID="ID_1281547563" MODIFIED="1474351336298" TEXT="Porting GDB">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
-<node COLOR="#990000" CREATED="1474349646051" ID="ID_725554885" LINK="docs/epgdb1.html" MODIFIED="1474351424205" TEXT="Porting GDB (1) arch and frame">
+<node COLOR="#990000" CREATED="1474349646051" ID="ID_725554885" LINK="docs/pgdb1.html" MODIFIED="1474351374977" TEXT="&#x79fb;&#x690d;GDB(1) arch&#x548c;frame v0.4">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
-<node COLOR="#990000" CREATED="1474349669043" ID="ID_221221096" LINK="docs/pgdb2.html" MODIFIED="1474351424384" TEXT="&#x79fb;&#x690d;GDB(2) native">
+<node COLOR="#990000" CREATED="1474349669043" ID="ID_221221096" LINK="docs/pgdb2.html" MODIFIED="1474351083315" TEXT="&#x79fb;&#x690d;GDB(2) native">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
-<node COLOR="#990000" CREATED="1474349687275" ID="ID_709259693" LINK="docs/pgdb3.html" MODIFIED="1474351424552" TEXT="&#x79fb;&#x690d;GDB(3) init.c&#x548c;target_ops">
+<node COLOR="#990000" CREATED="1474349687275" ID="ID_709259693" LINK="docs/pgdb3.html" MODIFIED="1474351296867" TEXT="&#x79fb;&#x690d;GDB(3) init.c&#x548c;target_ops">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
-<node COLOR="#990000" CREATED="1474349698563" ID="ID_1321333802" LINK="docs/pgdb4.html" MODIFIED="1474351424728" TEXT="&#x79fb;&#x690d;GDB(4) gdbserver">
+<node COLOR="#990000" CREATED="1474349698563" ID="ID_1321333802" LINK="docs/pgdb4.html" MODIFIED="1474351307763" TEXT="&#x79fb;&#x690d;GDB(4) gdbserver">
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
@@ -129,15 +129,15 @@
 <font NAME="SansSerif" SIZE="16"/>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1472709817072" ID="ID_1649698738" LINK="https://github.com/teawater" MODIFIED="1472804071736" POSITION="left" TEXT="My github">
+<node COLOR="#0033ff" CREATED="1472709817072" ID="ID_1649698738" LINK="https://github.com/teawater" MODIFIED="1472804205576" POSITION="left" TEXT="&#x6211;&#x7684;github">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1472709833438" ID="ID_851068096" LINK="http://www.linkedin.com/in/teawater" MODIFIED="1472804071739" POSITION="right" TEXT="My Linkedin">
+<node COLOR="#0033ff" CREATED="1472709833438" ID="ID_851068096" LINK="http://www.linkedin.com/in/teawater" MODIFIED="1472804205577" POSITION="right" TEXT="&#x6211;&#x7684;Linkedin">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
 </node>
-<node COLOR="#0033ff" CREATED="1474342066323" ID="ID_1594024145" LINK="https://500px.com/teawater" MODIFIED="1474344978930" POSITION="left" TEXT="My photo">
+<node COLOR="#0033ff" CREATED="1474342066323" ID="ID_1594024145" LINK="https://500px.com/teawater" MODIFIED="1474342081128" POSITION="left" TEXT="&#x6211;&#x7684;&#x7167;&#x7247;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
 </node>
---
+++ indexen.mm
@@ -0,0 +1,145 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1472708408515" ID="ID_662009073" MODIFIED="1473754198367" TEXT="teawater(Hui Zhu)&apos;s Homepage">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1472708752837" ID="ID_583425608" LINK="index.html" MODIFIED="1474361655110" POSITION="right" TEXT="&#x4e2d;&#x6587;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1472708596511" ID="ID_1638893491" MODIFIED="1472804071716" POSITION="left" TEXT="My Projects">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1474351738446" ID="ID_487449301" LINK="https://lkml.org/lkml/2016/8/22/151" MODIFIED="1474351845114" TEXT="Make ZRAM just store the high compression rate page">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1472708440400" ID="ID_756456714" LINK="http://teawater.github.io/kgtp/" MODIFIED="1474351435507" TEXT="KGTP">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1472708553887" ID="ID_477053151" MODIFIED="1474351425093" TEXT="KGTP is a comprehensive dynamic tracer for analysing Linux kernel and application (including Android) problems on production systems in real time.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1474346942731" ID="ID_207662382" MODIFIED="1474347490133" TEXT="GDB Reverse Debug and Process Record">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1474347420888" ID="ID_905890041" LINK="https://www.gnu.org/software/gdb/news/reversible.html" MODIFIED="1474347453132" TEXT="GDB and Reverse Debugging">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1474347428520" ID="ID_1354850590" LINK="https://sourceware.org/gdb/wiki/ProcessRecord" MODIFIED="1474347465059" TEXT="Process Record and Replay">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1474347438184" ID="ID_1211499164" LINK="prec/prec.pdf" MODIFIED="1474347479591" TEXT="slide">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1474338938608" ID="ID_575319148" MODIFIED="1474351440905" TEXT="2006: Skyeye DBCT">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1474339609600" ID="ID_949165" LINK="dbct/skyeye_dbct_doc.html" MODIFIED="1474340772023" TEXT="SKYEYE&#x6307;&#x4ee4;&#x52a8;&#x6001;&#x7ffb;&#x8bd1;&#x6a21;&#x62df;(DBCT)&#x5b9e;&#x73b0;&#x4ecb;&#x7ecd;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1474339616296" ID="ID_1870873388" LINK="dbct/skyeye_dbct_presentation.html" MODIFIED="1474340925791" TEXT="SKYEYE&#x6307;&#x4ee4;&#x52a8;&#x6001;&#x7ffb;&#x8bd1;&#x6a21;&#x62df;&#x8bb2;&#x5ea7;&#x63d0;&#x7eb2;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1472708672687" ID="ID_1029802733" LINK="http://teawater.github.io/get-gdb/" MODIFIED="1472804071730" TEXT="Get-GDB">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1472708696477" ID="ID_420345347" MODIFIED="1472804071731" TEXT="A Python script to auto check version, download, build and install specified version GDB to current system.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1472708619957" ID="ID_1905391322" LINK="http://teawater.github.io/cma/" MODIFIED="1474341750911" TEXT="CMA">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1472708659623" ID="ID_438199411" MODIFIED="1472804071729" TEXT="A GDB Python script that analyzes and records C/C++ application&apos;s dynamic memory status. ">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1473754199442" ID="ID_452624323" MODIFIED="1473754251968" POSITION="right" TEXT="My presentation">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1473754297417" ID="ID_823409592" MODIFIED="1474168607106" TEXT="2014: Buddy&#x548c;CMA&#x7b80;&#x4ecb; &#x4ee5;&#x53ca;&#x5728;Android&#x4e2d;&#x5b9e;&#x9645;&#x4f7f;&#x7528; CMA&#x9047;&#x5230;&#x95ee;&#x9898;&#x7684;&#x6539;&#x8fdb;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1473754338481" ID="ID_730958682" LINK="presentation/2014clk_cma.pdf" MODIFIED="1473754376673" TEXT="slide">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1473831335355" ID="ID_120183860" MODIFIED="1473831339091" TEXT="video">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1473755069350" ID="ID_260329777" LINK="https://youtu.be/jMhDBTdpfG0" MODIFIED="1473831349843" TEXT="youtube">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1473831319792" ID="ID_225778867" LINK="http://v.youku.com/v_show/id_XMTcyMzk5Mzc4MA==.html" MODIFIED="1473831349844" TEXT="youku">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1472709297884" ID="ID_1226999614" MODIFIED="1472804071734" POSITION="right" TEXT="My mindmap">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1474168934713" ID="ID_1441026216" MODIFIED="1474168940500" TEXT="&#x7b80;&#x7ea6;&#x4e4b;&#x7f8e;&#xff1a;&#x8f6f;&#x4ef6;&#x8bbe;&#x8ba1;&#x4e4b;&#x9053; &#x8bfb;&#x4e66;&#x7b14;&#x8bb0;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1472709684271" ID="ID_1955654431" LINK="mindmap/Code_Simplicity.html" MODIFIED="1474168961512" TEXT="html">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1474168987710" ID="ID_317346601" LINK="mindmap/Code_Simplicity.mm" MODIFIED="1474173135819" TEXT="freemind">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1474361006979" ID="ID_1114785013" MODIFIED="1474361057052" TEXT=" &#x4e0d;&#x61c2;&#x5e26;&#x4eba;&#xff0c;&#x4f60;&#x5c31;&#x81ea;&#x5df1;&#x5e72;&#x5230;&#x6b7b; &#x8bfb;&#x4e66;&#x7b14;&#x8bb0;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1472709684271" ID="ID_822489091" LINK="mindmap/tst.html" MODIFIED="1474361090903" TEXT="html">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1474168987710" ID="ID_803063790" LINK="mindmap/tst.mm" MODIFIED="1474361099383" TEXT="freemind">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1472803961041" ID="ID_784008993" MODIFIED="1472804134439" POSITION="left" TEXT="My documents">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1474349523443" ID="ID_1281547563" MODIFIED="1474349533374" TEXT="Porting GDB">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1474349646051" ID="ID_725554885" LINK="docs/epgdb1.html" MODIFIED="1474351424205" TEXT="Porting GDB (1) arch and frame">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1474349669043" ID="ID_221221096" LINK="docs/pgdb2.html" MODIFIED="1474351424384" TEXT="&#x79fb;&#x690d;GDB(2) native">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1474349687275" ID="ID_709259693" LINK="docs/pgdb3.html" MODIFIED="1474351424552" TEXT="&#x79fb;&#x690d;GDB(3) init.c&#x548c;target_ops">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1474349698563" ID="ID_1321333802" LINK="docs/pgdb4.html" MODIFIED="1474351424728" TEXT="&#x79fb;&#x690d;GDB(4) gdbserver">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1472803992865" ID="ID_1742802608" LINK="docs/p4a.html" MODIFIED="1472804071738" TEXT="&#x5728;&#x5b89;&#x5353;&#x5e73;&#x53f0;&#x4e0a;&#x4f7f;&#x7528;Python&#x7684;&#x65b9;&#x6cd5; ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1472709817072" ID="ID_1649698738" LINK="https://github.com/teawater" MODIFIED="1472804071736" POSITION="left" TEXT="My github">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1472709833438" ID="ID_851068096" LINK="http://www.linkedin.com/in/teawater" MODIFIED="1472804071739" POSITION="right" TEXT="My Linkedin">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1474342066323" ID="ID_1594024145" LINK="https://500px.com/teawater" MODIFIED="1474344978930" POSITION="left" TEXT="My photo">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
